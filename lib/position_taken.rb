d3a4d8319cf63bfb6accedb8fc02a7398c4929d4
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-06-18 14:30:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 board = ["", "X", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board,index)
-  index.to_i -1= index_number
-  if board[index_number] == "X" || board[index_number] == "O"
+  if board[(index.to_i -1)] == "X" || board[index.to_i -1] == "O"
     return false
   else
     return true
