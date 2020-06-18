c03e5e167c4fa82449b8c071d2adf70e490ba2f0
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-06-18 13:20:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+board = ["", "X", " ", " ", " ", " ", " ", " ", " "]
+def position_taken?(board,index)
+  index.to_i -1= index_number
+  if board[index_number] == nil
+    false
+  elsif board[index_number] =="X"
+    true
+  end
+end
